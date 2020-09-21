f24a989641649f99c2a27cfd7510fab0f76d1574
ShingoMisawa <33051481+shngmsw@users.noreply.github.com>
2020-09-22 02:35:22 +0900

    update ban command to v12

--- app/ban.js
+++ app/ban.js
@@ -9,29 +9,26 @@ module.exports = async function handleBan(msg) {
         strCmd = strCmd.replace("\x20+", " ");
         const args = strCmd.split(" ");
         args.shift();
-        const user = await msg.mentions.members.first();
+        const member = await msg.mentions.members.first();
         let id = args[0];
         console.log(id);
         if (args[1]) {
-            let reason =
+            let reasonText =
                 "イカ部の管理人です。以下の理由によりイカ部から退部とさせていただきました。```" +
                 args[1] +
                 "```" +
                 "申し訳ありませんが、質問等は受け付けておりませんので、よろしくお願いいたします。";
 
-            user.createDM().then(DMChannel => {
-                // We have now a channel ready.
-                // Send the message.
-                DMChannel.send(reason).then(async () => {
-                    // Message sent, time to kick.
-                    const banmember = await msg.guild.members.ban(user.id, reason);
-                    console.log(banmember);
-                    msg.guild.channels.cache.find(channel => channel.name === "banコマンド").send(
-                        `${banmember.username}さんを以下の理由によりBANしました。\n` +
-                        reason
-                    );
-                });
-            });
+            let DMChannel = await member.createDM();
+            await DMChannel.send(reasonText)
+
+            await member.ban({ reason: reasonText }).catch(console.error);
+
+            const banChannel = msg.guild.channels.cache.find(channel => channel.name === "banコマンド")
+            banChannel.send(
+                `${member.user.tag}さんを以下の理由によりBANしました。\n` +
+                reasonText
+            );
         } else {
             return msg.channel.send("理由を入れるでし！");
         }
