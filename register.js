01d57c3c720c3e2330484d7eae2828715e7453e1
shngmsw <mose1021@gmail.com>
2022-06-26 17:38:40 +0900

    add delete commands all

--- register.js
+++ register.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require(`@discordjs/builders`);
 require('dotenv').config();
-
 const voiceLock = new SlashCommandBuilder()
     .setName('voice_channel')
     .setDescription('ボイスチャンネルの人数制限を設定します。')
@@ -19,7 +18,20 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
 async function main() {
-    await rest.put(Routes.applicationCommands(process.env.DISCORD_BOT_ID), { body: commands });
+    await rest
+        .put(Routes.applicationCommands(process.env.DISCORD_BOT_ID), { body: [] })
+        .then(() => console.log('Successfully deleted application commands.'))
+        .catch(console.error);
+
+    await rest
+        .put(Routes.applicationGuildCommands(process.env.DISCORD_BOT_ID, process.env.SERVER_ID), { body: [] })
+        .then(() => console.log('Successfully deleted application guild commands.'))
+        .catch(console.error);
+
+    await rest
+        .put(Routes.applicationGuildCommands(process.env.DISCORD_BOT_ID, process.env.SERVER_ID), { body: commands })
+        .then(() => console.log('Successfully registered application guild commands.'))
+        .catch(console.error);
 }
 
 main().catch((err) => console.log(err));
