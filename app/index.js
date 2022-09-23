47bc9699d254e42cd632988710b217cbd16f19e2
√lump <73431007+root-lump@users.noreply.github.com>
2022-09-24 00:57:59 +0900

    bug fix

--- app/index.js
+++ app/index.js
@@ -43,7 +43,7 @@ const { handleFriendCode, deleteFriendCode } = require(app + '/cmd/other/friendc
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 client.on('messageCreate', async (msg) => {
-    const member = await msg.guild.members.cache.get(msg.author.id);
+    const member = msg.guild.members.cache.get(msg.author.id);
     if (msg.author.bot || member.permissions.has('MANAGE_CHANNELS')) {
         if (msg.content.startsWith('/poll')) {
             if (msg.author.username === 'ブキチ') {
