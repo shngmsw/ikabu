3adb9cfdd96b5fe3a5067125bd7fbc993d97f599
√lump <73431007+root-lump@users.noreply.github.com>
2023-01-03 19:56:58 +0900

    fix: キャンバス変更漏れ

--- app/canvas/recruit/fes_canvas.js
+++ app/canvas/recruit/fes_canvas.js
@@ -49,46 +49,35 @@ async function recruitFesCanvas(recruit_num, count, host_member, user1, user2, t
     recruit_ctx.stroke();
     recruit_ctx.restore();
 
-    let user1_url = blank_avatar_url;
-    let user2_url = blank_avatar_url;
-    let user3_url = blank_avatar_url;
+    let member_urls = [];
 
-    // 参加者指定があれば、画像を拾ってくる
-    if (user1 != null && user2 != null) {
-        user1_url = user1.displayAvatarURL({ extension: 'png' });
-        user2_url = user2.displayAvatarURL({ extension: 'png' });
-    } else if (user1 != null && user2 == null) {
-        user1_url = user1.displayAvatarURL({ extension: 'png' });
-    } else if (user1 == null && user2 != null) {
-        user1_url = user2.displayAvatarURL({ extension: 'png' });
+    if (user1 === 'dummy_icon') {
+        member_urls.push(
+            'http://placehold.jp/30/3d4070/ffffff/150x150.png?text=%E8%A1%A8%E7%A4%BA%E3%81%99%E3%82%8B%E3%81%AB%E3%81%AF%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E3%81%A7%E5%8B%9F%E9%9B%86',
+        );
+    } else if (user1 != null) {
+        member_urls.push(user1.displayAvatarURL({ extension: 'png' }));
     }
 
-    let user1_img = await Canvas.loadImage(user1_url);
-    recruit_ctx.save();
-    drawArcImage(recruit_ctx, user1_img, 158, 120, 50);
-    recruit_ctx.strokeStyle = '#1e1f23';
-    recruit_ctx.lineWidth = 9;
-    recruit_ctx.stroke();
-    recruit_ctx.restore();
-
-    if (count >= 3) {
-        let user2_img = await Canvas.loadImage(user2_url);
-        recruit_ctx.save();
-        drawArcImage(recruit_ctx, user2_img, 276, 120, 50);
-        recruit_ctx.strokeStyle = '#1e1f23';
-        recruit_ctx.lineWidth = 9;
-        recruit_ctx.stroke();
-        recruit_ctx.restore();
+    if (user2 === 'dummy_icon') {
+        member_urls.push(
+            'http://placehold.jp/30/3d4070/ffffff/150x150.png?text=%E8%A1%A8%E7%A4%BA%E3%81%99%E3%82%8B%E3%81%AB%E3%81%AF%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E3%81%A7%E5%8B%9F%E9%9B%86',
+        );
+    } else if (user2 != null) {
+        member_urls.push(user2.displayAvatarURL({ extension: 'png' }));
     }
 
-    if (count == 4) {
-        let user3_img = await Canvas.loadImage(user3_url);
-        recruit_ctx.save();
-        drawArcImage(recruit_ctx, user3_img, 394, 120, 50);
-        recruit_ctx.strokeStyle = '#1e1f23';
-        recruit_ctx.lineWidth = 9;
-        recruit_ctx.stroke();
-        recruit_ctx.restore();
+    for (let i = 0; i < 4; i++) {
+        if (count >= i + 2) {
+            let user_url = member_urls[i] != null ? member_urls[i] : blank_avatar_url;
+            let user_img = await Canvas.loadImage(user_url);
+            recruit_ctx.save();
+            drawArcImage(recruit_ctx, user_img, i * 118 + 158, 120, 50);
+            recruit_ctx.strokeStyle = '#1e1f23';
+            recruit_ctx.lineWidth = 9;
+            recruit_ctx.stroke();
+            recruit_ctx.restore();
+        }
     }
 
     let host_icon = await Canvas.loadImage('https://raw.githubusercontent.com/shngmsw/ikabu/main/images/recruit/squid.png');
