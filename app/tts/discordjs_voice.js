e3a87d651dc5b7c11a1bfafa751f2e10d141e347
shngmsw <mose1021@gmail.com>
2023-01-28 14:27:46 +0900

    fix:check subscription

--- app/tts/discordjs_voice.js
+++ app/tts/discordjs_voice.js
@@ -9,6 +9,7 @@ const {
     NoSubscriberBehavior,
     generateDependencyReport,
 } = require('@discordjs/voice');
+const { isNotEmpty } = require('../common.js');
 const log4js = require('log4js');
 
 log4js.configure(process.env.LOG4JS_CONFIG_PATH);
@@ -107,7 +108,7 @@ async function play(msg) {
     try {
         const { guildId, channelId } = msg;
         let subscription = subscriptions.get(guildId);
-        if (subscription && channels.get(guildId) === channelId) {
+        if (isNotEmpty(subscription) && channels.get(guildId) === channelId) {
             // メッセージから音声ファイルを取得
             const buffer = await mode_api(msg);
             if (buffer == null) return;
