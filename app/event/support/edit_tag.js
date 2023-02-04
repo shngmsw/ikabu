4987fc32de6086b3e2644d8c711d3580de6ab96a
√lump <73431007+root-lump@users.noreply.github.com>
2023-02-04 18:23:13 +0900

    fix: return忘れ修正 & try-catch追加

--- app/event/support/edit_tag.js
+++ app/event/support/edit_tag.js
@@ -1,17 +1,24 @@
 const { isEmpty } = require('../../common');
 const { tagIdsEmbed } = require('./tag_ids_embed');
+const log4js = require('log4js');
 
 module.exports = {
     editThreadTag: editThreadTag,
 };
 
-async function editThreadTag(thread) {
-    if (isEmpty(process.env.TAG_ID_SUPPORT_PROGRESS) || isEmpty(process.env.TAG_ID_SUPPORT_RESOLVED)) {
-        await thread.send({ embeds: [tagIdsEmbed(thread)] });
-        return;
-    }
+const logger = log4js.getLogger('default');
 
-    let appliedTags = thread.appliedTags;
-    appliedTags.push(process.env.TAG_ID_SUPPORT_PROGRESS);
-    await thread.setAppliedTags(appliedTags, '質問対応開始');
+async function editThreadTag(thread) {
+    try {
+        if (isEmpty(process.env.TAG_ID_SUPPORT_PROGRESS) || isEmpty(process.env.TAG_ID_SUPPORT_RESOLVED)) {
+            await thread.send({ embeds: [tagIdsEmbed(thread)] });
+            return;
+        }
+
+        let appliedTags = thread.appliedTags;
+        appliedTags.push(process.env.TAG_ID_SUPPORT_PROGRESS);
+        await thread.setAppliedTags(appliedTags, '質問対応開始');
+    } catch (error) {
+        logger.error(error);
+    }
 }
--- app/event/support/tag_ids_embed.js
+++ app/event/support/tag_ids_embed.js
@@ -1,19 +1,26 @@
 const { EmbedBuilder } = require('discord.js');
+const log4js = require('log4js');
 
 module.exports = {
     tagIdsEmbed: tagIdsEmbed,
 };
 
+const logger = log4js.getLogger('default');
+
 function tagIdsEmbed(thread) {
-    let description = '管理者は環境変数に対応中タグのIDと回答済みタグのIDを設定するでし！\n';
+    try {
+        let description = '管理者は環境変数に対応中タグのIDと回答済みタグのIDを設定するでし！\n';
 
-    const tags = thread.parent.availableTags;
-    for (let tag of tags) {
-        description = description + tag.name + ': `' + tag.id + '`\n';
+        const tags = thread.parent.availableTags;
+        for (let tag of tags) {
+            description = description + tag.name + ': `' + tag.id + '`\n';
+        }
+
+        const embed = new EmbedBuilder();
+        embed.setTitle('サポートセンタータグIDの設定');
+        embed.setDescription(description);
+        return embed;
+    } catch (error) {
+        logger.error(error);
     }
-
-    const embed = new EmbedBuilder();
-    embed.setTitle('サポートセンタータグIDの設定');
-    embed.setDescription(description);
-    return embed;
 }
