12337b0f697cfa69e83e28d289d889abe20f926a
root-lump <apple.iphone5.ios6.2@gmail.com>
2023-06-07 02:42:07 +0900

    fix: updateSchedule時のエラー修正

--- src/app/common/apis/splatoon3_ink.ts
+++ src/app/common/apis/splatoon3_ink.ts
@@ -68,7 +68,7 @@ export async function updateSchedule() {
 
         storageCache.set('sp3_schedule', schduleData.data);
         logger.info('schedule fetched!');
-        return schduleData;
+        return schduleData.data;
     } catch (error) {
         logger.error(error);
     }
