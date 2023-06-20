ac0a0bf154da2fc2c0ec1b906e50501295348762
shngmsw <mose1021@gmail.com>
2023-06-20 22:32:05 +0900

    refactor: cz-configから絵文字を削除
    undefined

--- .cz-config.js
+++ .cz-config.js
@@ -1,42 +1,42 @@
 module.exports = {
   types: [
     {
-      value: "🌟 feat",
+      value: "feat",
       name: "feat: 機能追加",
       title: "Features",
     },
     {
-      value: "🔧 fix",
+      value: "fix",
       name: "fix: バグの修正",
       title: "Bug Fixes",
     },
     {
-      value: "🗒 docs",
+      value: "docs",
       name: "docs: ドキュメントのみの変更",
       title: "Documentation",
     },
     {
-      value: "🎨 style",
+      value: "style",
       name: "style: コードの動作に影響しない、見た目だけの変更（スペース、フォーマット、欠落の修正、セミコロンなど)",
       title: "Styles",
     },
     {
-      value: "♻️ refactor",
+      value: "refactor",
       name: "refactor: バグの修正や機能の追加ではないコードの変更",
       title: "Code Refactoring",
     },
     {
-      value: "⏫　perf",
+      value: "perf",
       name: "perf: パフォーマンスを向上させるコードの変更",
       title: "Performance",
     },
     {
-      value: "🧪 test",
+      value: "test",
       name: "test: 不足しているテストの追加や既存のテストの修正",
       title: "Tests",
     },
     {
-      value: "🐧 chore",
+      value: "chore",
       name: "chore: ビルドプロセスやドキュメント生成などの補助ツールやライブラリの変更",
       title: "Chores",
     },
