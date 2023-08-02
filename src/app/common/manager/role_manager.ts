74591a5fca0cd14ab98965c6dec99d662e3bd7d7
root-lump <apple.iphone5.ios6.2@gmail.com>
2023-08-03 01:22:04 +0900

    refactor: role_managerにassgin/unassignを整備

--- src/app/common/manager/role_manager.ts
+++ src/app/common/manager/role_manager.ts
@@ -61,15 +61,65 @@ export async function searchRoleById(guild: Guild, roleId: string) {
 }
 
 /**
- * 指定のロールをメンバーから解除する
- * @param roleId 解除するロールID
+ * 指定のロールを特定のメンバーに付与する
+ * @param roleIdOrRole 付与するロールID or Roleオブジェクト
+ * @param member GuildMemberオブジェクト
+ * @returns 成功したかどうかを返す
+ */
+export async function assignRoleToMember(roleIdOrRole: string | Role, member: GuildMember) {
+    try {
+        await member.roles.add(roleIdOrRole);
+        return true;
+    } catch (error) {
+        logger.error(error);
+        return false;
+    }
+}
+
+/**
+ * 指定のロールを特定のメンバーから解除する
+ * @param roleIdOrRole 解除するロールID or Roleオブジェクト
+ * @param member GuildMemberオブジェクト
+ * @returns 成功したかどうかを返す
+ */
+export async function unassignRoleFromMember(roleIdOrRole: string | Role, member: GuildMember) {
+    try {
+        await member.roles.remove(roleIdOrRole);
+        return true;
+    } catch (error) {
+        logger.error(error);
+        return false;
+    }
+}
+
+/**
+ * 指定のロールをメンバーに付与する
+ * @param roleIdOrRole 付与するロールID or Roleオブジェクト
  * @param members メンバーコレクション
  * @returns 成功したかどうかを返す
  */
-export async function unassginRoleFromMembers(roleId: string, members: Collection<string, GuildMember>) {
+export async function assginRoleToMembers(roleIdOrRole: string | Role, members: Collection<string, GuildMember>) {
     try {
         members.forEach(async (member: GuildMember) => {
-            await member.roles.remove(roleId);
+            await member.roles.add(roleIdOrRole);
+        });
+        return true;
+    } catch (error) {
+        logger.error(error);
+        return false;
+    }
+}
+
+/**
+ * 指定のロールをメンバーから解除する
+ * @param roleIdOrRole 解除するロールID or Roleオブジェクト
+ * @param members メンバーコレクション
+ * @returns 成功したかどうかを返す
+ */
+export async function unassginRoleFromMembers(roleIdOrRole: string | Role, members: Collection<string, GuildMember>) {
+    try {
+        members.forEach(async (member: GuildMember) => {
+            await member.roles.remove(roleIdOrRole);
         });
         return true;
     } catch (error) {
@@ -101,6 +151,8 @@ export async function setColorToRole(guild: Guild, role: Role, color?: string | 
     }
 }
 
+// 余計な処理をしているので非推奨 そのうち直す
+// assignRoleToSpecificMember or assignRoleToMembers 推奨
 /**
  * メンバーにロールを付与するコマンド，メンバーが見つからない場合エラーメッセージを返す
  * @param {Guild} guild Guildオブジェクト
