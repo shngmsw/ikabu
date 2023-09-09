e97681be28d25c8a672681dd14d0a09e6ef64020
root-lump <apple.iphone5.ios6.2@gmail.com>
2023-09-10 06:11:50 +0900

    update: VCToolsとAdminチャンネルのチェックをDBの情報で行うように変更

--- src/app/feat-admin/environment_variables/variables_handler.ts
+++ src/app/feat-admin/environment_variables/variables_handler.ts
@@ -1,29 +1,36 @@
-import { ChatInputCommandInteraction, Guild, PermissionsBitField } from 'discord.js';
+import { ChatInputCommandInteraction, PermissionsBitField } from 'discord.js';
 
 import { deleteVariables } from './delete_variables';
 import { setVariables } from './set_variables';
 import { showVariables } from './show_variables';
-import { searchChannelById } from '../../common/manager/channel_manager';
+import { ChannelService } from '../../../db/channel_service';
 import { getGuildByInteraction } from '../../common/manager/guild_manager';
-import { searchAPIMemberById } from '../../common/manager/member_manager';
-import { assertExistCheck, exists, notExists } from '../../common/others';
+import { getAPIMemberByInteraction } from '../../common/manager/member_manager';
+import { notExists } from '../../common/others';
 
 export async function variablesHandler(interaction: ChatInputCommandInteraction<'cached' | 'raw'>) {
     await interaction.deferReply({ ephemeral: true });
     const guild = await getGuildByInteraction(interaction);
-    const member = await searchAPIMemberById(guild, interaction.member.user.id);
-    assertExistCheck(member, 'member');
+    const member = await getAPIMemberByInteraction(interaction);
+
     if (!member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
         return await interaction.editReply({
             content: 'チャンネルを管理する権限がないでし！',
         });
     }
 
-    if (
-        exists(interaction.channel) &&
-        interaction.channel.parentId !== process.env.CATEGORY_PARENT_ID_ADMIN_ONLY
-    ) {
-        return sendChannelError(interaction, guild);
+    const dbChannel = await ChannelService.getChannel(guild.id, interaction.channelId);
+
+    if (notExists(dbChannel)) {
+        return await interaction.editReply({
+            content: 'このチャンネルの情報を取得できないでし！',
+        });
+    }
+
+    if (!dbChannel.isAdminChannel) {
+        return await interaction.editReply({
+            content: 'このチャンネルでは使えないでし！',
+        });
     }
 
     switch (interaction.options.getSubcommand()) {
@@ -36,32 +43,5 @@ export async function variablesHandler(interaction: ChatInputCommandInteraction<
         case '削除':
             void deleteVariables(interaction);
             break;
-    }
-}
-
-async function sendChannelError(
-    interaction: ChatInputCommandInteraction<'cached' | 'raw'>,
-    guild: Guild,
-) {
-    const envChannelId = process.env.CATEGORY_PARENT_ID_ADMIN_ONLY;
-    if (notExists(envChannelId)) {
-        // .envに記載がない場合
-        await interaction.editReply({
-            content: '`CATEGORY_PARENT_ID_ADMIN_ONLY`が正しく設定されているか確認するでし！',
-        });
-    } else {
-        // .envに記載がある場合
-        const envChannel = await searchChannelById(guild, envChannelId);
-        if (notExists(envChannel)) {
-            // .envのカテゴリIDが間違っている場合
-            await interaction.editReply({
-                content: '`CATEGORY_PARENT_ID_ADMIN_ONLY`が正しく設定されているか確認するでし！',
-            });
-        } else {
-            // .envで指定されたカテゴリと一致しない場合
-            await interaction.editReply({
-                content: 'このカテゴリでは使えないでし！',
-            });
-        }
     }
 }
