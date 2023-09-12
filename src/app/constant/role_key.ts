73e3430de503c0083b6166b20fcbf7113ed020c1
root-lump <apple.iphone5.ios6.2@gmail.com>
2023-09-13 06:36:49 +0900

    refactor: ロールキーの追加と.env.sampleからの削除

--- src/app/constant/role_key.ts
+++ src/app/constant/role_key.ts
@@ -2,7 +2,17 @@ import { ObjectValueList } from './constant_common';
 
 // 固有の機能ロールを増やすときはここに追加するだけでOK
 export const RoleKeySet = {
-    test: { name: 'テストロール', key: 'ROLE_ID_TEST' },
+    Developer: { name: 'DEVELOPER', key: 'ROLE_ID_DEVELOPER' },
+    Rookie: { name: '新入部員', key: 'ROLE_ID_ROOKIE' },
+    PrivateRecruit: { name: 'プラベ募集', key: 'ROLE_ID_RECRUIT_PRIVATE' },
+    RegularRecruit: { name: 'ナワバリ募集', key: 'ROLE_ID_RECRUIT_REGULAR' },
+    AnarchyRecruit: { name: 'バンカラ募集', key: 'ROLE_ID_RECRUIT_ANARCHY' },
+    EventRecruit: { name: 'イベマ募集', key: 'ROLE_ID_RECRUIT_EVENT' },
+    SalmonRecruit: { name: 'サーモン募集', key: 'ROLE_ID_RECRUIT_SALMON' },
+    FryeRecruit: { name: 'ウツホ募集', key: 'ROLE_ID_RECRUIT_FRYE' },
+    ShiverRecruit: { name: 'フウカ募集', key: 'ROLE_ID_RECRUIT_SHIVER' },
+    BigmanRecruit: { name: 'マンタロー募集', key: 'ROLE_ID_RECRUIT_BIGMAN' },
+    OtherGamesRecruit: { name: '別ゲー募集', key: 'ROLE_ID_RECRUIT_OTHERGAMES' },
 } as const;
 export type RoleKeySet = ObjectValueList<typeof RoleKeySet>;
 // RoleKeyタイプを派生させる
